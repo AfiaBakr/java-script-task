d79fad7b5c53de15bf244de18734832d62881e55
AfiaBakr <afiabakr8602@gmail.com>
2025-08-01 00:33:32 +0500

    JavaScript task

---
+++ chapter-01-to-05/app.js
@@ -0,0 +1,205 @@
+//--CHAPTER 01--//
+
+//___________________Chap-01 Question 01_____________/
+// alert("Welcome to My Web page")
+
+//___________________Chap-01 Question 02_____________/
+// alert("Error! Please entr a valid password")
+
+//___________________Chap-01 Question 03_____________/
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+//___________________Chap-01 Question 04_____________/
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent the page from create additional dialogs.")
+
+//___________________Chap-01 Question 05_____________/
+// alert("Hello... Ican run JS through my web browser's console")
+
+//___________________Chap-01 Question 06_____________/
+// alert("Welcome to My JS Web page through my web browser's console")
+
+//___________________Chap-01 Question 06_____________/
+// alert("Welcome to My JS Web page through my web browser's console")
+
+//------------------------------END-------------------------------------//
+
+//--CHAPTER 02--//
+
+//___________________Chap-02 Question 01_____________/
+// var username = "Afia"
+// alert(username)
+
+//___________________Chap-02 Question 02_____________/
+// var myName = "Afia Bakr"
+// alert(myName)
+
+//___________________Chap-02 Question 03_____________/
+// var message = "Hello World"
+// alert(message)
+
+//___________________Chap-02 Question 04_____________/
+// var studentName = "Afia Bakr"
+// var studentAge = "15 years old"
+// var studentCourse = "Certified Mobile Application Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(studentCourse)
+
+//___________________Chap-02 Question 05_____________/
+// var pizzaName = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizzaName)
+
+//___________________Chap-02 Question 06_____________/
+// var email = "My email address is example@example.com"
+// alert(email)
+
+//___________________Chap-02 Question 07_____________/
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+//___________________Chap-02 Question 08_____________/
+// document.write("Yah! I can write HTML content through JavaScript")
+
+//___________________Chap-02 Question 02_____________/
+// alert("=========🀁🏫🀥🏫🀁=========")
+
+//___________________Chap-02 Question 09_____________/
+// alert("Yah! I can write HTML content through JavaScript")
+
+//------------------------------END-------------------------------------//
+
+//--CHAPTER 03--//
+
+//___________________Chap-03 Question 01_____________/
+// var age = 15;
+// alert("I am " +age+ " years old")
+
+//___________________Chap-03 Question 02_____________/
+// alert("You have visited this site 14 times")
+
+//___________________Chap-03 Question 03_____________/
+// var birthYear = 1990;
+// document.write("My birth year is " +birthYear+ "<br/>Data type of my declaration is "+typeof birthYear)
+
+//___________________Chap-03 Question 04_____________/
+// var clothingStore = "XYZ Clothing store."
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt(s)";
+// var quantity= 15;
+// document.write(visitorName +" ordered " +quantity+" " +productTitle+" on "+clothingStore)
+
+//------------------------------END-------------------------------------//
+
+//--CHAPTER 04--//
+
+//___________________Chap-04 Question 01_____________/
+// var myName = "My name is "
+// var firstName ="Afia "
+// var lastName = "Bakr";
+
+// alert(myName +firstName+ lastName)
+
+//___________________Chap-04 Question 02_____________/
+//----------legal var name-----//
+// var myName = "My name is "
+// var Name1 ="Afia "
+// var _name = "Bakr";
+// var $name = "Bakr";
+// var myFirstName = "Afia";
+
+//----------legal var name-----//
+// var MyName = "My name is "
+// var 1Name ="Afia "
+// var alert = "Afia";
+// var /name = "Bakr";
+// var my First Name = "Afia";
+
+//___________________Chap-04 Question 03_____________/
+// document.write(
+//   "<b>Rules for naming JS variables</b> <br/><br/><br/><br/>Variable names can only contain, numbers, $ and _. For example <b>$my_1stVariable</b> <br/>Variables must begin with a letter, $ or _. For example <b>$name, _name or name</b> <br/>Variable names are case sensitive <br/>Variable names should not be JS keywords"
+// );
+
+//------------------------------END-------------------------------------//
+
+//--CHAPTER 05--//
+
+//___________________Chap-05 Question 01_____________/
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2;
+// document.write("Sum of " +num1+ " and "+num2+" is "+sum)
+
+//___________________Chap-05 Question 02_____________/
+// var num1 = 3;
+// var num2 = 5;
+// var subtract = num1 - num2;
+// document.write("Sum of " +num1+ " and "+num2+" is "+subtract)
+
+// var num1 = 3;
+// var num2 = 5;
+// var multipy = num1 * num2;
+// document.write("<br/>Sum of " +num1+ " and "+num2+" is "+multipy)
+
+// var num1 = 3;
+// var num2 = 5;
+// var divide = num1 / num2;
+// document.write("<br/>Sum of " +num1+ " and "+num2+" is "+divide)
+
+// var num1 = 3;
+// var num2 = 5;
+// var remainder = num1 % num2;
+// document.write("<br/>Sum of " +num1+ " and "+num2+" is "+remainder)
+
+//___________________Chap-05 Question 03_____________/
+// var num1 
+// document.write("Value after declaration is "+num1)
+// num1 = 5;
+// document.write("<br/>Initial value: " +num1)
+// num1 ++
+// document.write("<br/>Value after increment is: "+num1)
+// num1 +=7
+// document.write("<br/>Value after addition is: "+num1)
+// num1 --
+// document.write("<br/>Value after decrement is: "+num1)
+// num1 %=3
+// document.write("<br/>The remainder is: "+num1)
+
+//___________________Chap-05 Question 04_____________/
+// var ticketPrice = 600
+// ticketPrice *=5
+// document.write("Total cost of buying 5 tickets to a movie is "+ticketPrice+"PKR")
+
+//___________________Chap-05 Question 05_____________/
+// var num1 = 4
+// document.write("Table of "+num1)
+// num1 *= 1;
+// document.write("<br/>4 X 1 = " +num1)
+// num2 = 4*2
+// document.write("<br/>4 X 2 = "+num2)
+// num3 = 4*3
+// document.write("<br/>4 X 3 = "+num3)
+// num4 = 4*4
+// document.write("<br/>4 X 4 = "+num4)
+// num5 = 4*5
+// document.write("<br/>4 X 5 = "+num5)
+// num6 = 4*6
+// document.write("<br/>4 X 6 = "+num6)
+// num7 = 4*7
+// document.write("<br/>4 X 7 = "+num7)
+// num8 = 4*8
+// document.write("<br/>4 X 8 = "+num8)
+// num9 = 4*9
+// document.write("<br/>4 X 9 = "+num9)
+// num10 = 4*10
+// document.write("<br/>4 X 10= "+num10)
+
+//___________________Chap-05 Question 06_____________/
+// var celsiusTemperature = 25
+// var convetFahrenheit = (celsiusTemperature * 9/5)+32
+// document.write(celsiusTemperature+"<sup>o</sup>C is "+convetFahrenheit+"<sup>o</sup>F")
+// var fahrenheitTemperature = 70
+// var convetCelsius = (fahrenheitTemperature - 32) * 5/9
+// document.write("<br/>"+fahrenheitTemperature+"<sup>o</sup>F is "+convetCelsius+"<sup>o</sup>C")
+
+//------------------------------END-------------------------------------//
